487ce402afaaf89f832b6b5646e3da46e84263a2
Nich0l3 <146418190+Nich0l3@users.noreply.github.com>
2024-05-15 23:56:52 +0530

    autostartup complete

--- startup.sh
+++ startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 SRC=$HOME'/smartMirrorV2'
-/usr/bin/chromium-browser --kiosk $SRC/display/index.html &
-sleep 1
-$SRC/voice-assistant/va/bin/python3 $SRC/voice-assistant/main.py 1>/dev/null 2>&1 &
-
+/usr/bin/firefox --new-window --kiosk $SRC/display/index.html
+sleep 3
+$SRC/voice-assistant/va/bin/python3 $SRC/voice-assistant/main.py  
